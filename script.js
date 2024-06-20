563771fd45baf06c56fd7f7ead71e23d78ea4f99
ErikaSantocono <162038047+ErikaSantocono@users.noreply.github.com>
2024-06-20 20:58:15 +0200

    update2

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
     let counter = 0;
     let isBinary = false;
-    const counterValueElement = document.getElementById("counter-value");
-    const decrementButton = document.getElementById("decrement-btn");
-    const incrementButton = document.getElementById("increment-btn");
-    const resetButton = document.getElementById("reset-btn");
+    const counterContainer = document.getElementById("counter-container");
+    const counterValueElement = document.createElement("span");
+    counterValueElement.id = "counter-value";
+    counterValueElement.textContent = counter;
+    const buttonsContainer = document.createElement("div");
+    
+    buttonsContainer.id = "buttons-container";
+    const decrementButton = document.createElement("button");
+    decrementButton.id = "decrement-btn";
+    decrementButton.textContent = "-";
+
+    const incrementButton = document.createElement("button");
+    incrementButton.id = "increment-btn";
+    incrementButton.textContent = "+";
+
+    const resetButton = document.createElement("button");
+    resetButton.id = "reset-btn";
+    resetButton.textContent = "Reset";
+
+    buttonsContainer.appendChild(decrementButton);
+    buttonsContainer.appendChild(incrementButton);
+    buttonsContainer.appendChild(resetButton);
+    counterContainer.appendChild(counterValueElement);
+    counterContainer.appendChild(buttonsContainer);
+
     const toggleBinaryCounter = document.getElementById("toggle-binary-counter");
 
-    function updateCounter() { counterValueElement.textContent = isBinary ? counter.toString(2) : counter;}
+    function updateCounter() {counterValueElement.textContent = isBinary ? counter.toString(2) : counter;}
 
-    decrementButton.addEventListener("click", function() { counter--; updateCounter();});
+    decrementButton.addEventListener("click", function() {counter--; updateCounter();});
 
     incrementButton.addEventListener("click", function() { counter++; updateCounter();});
 
-    resetButton.addEventListener("click", function() { counter = 0; updateCounter();});
+    resetButton.addEventListener("click", function() {counter = 0; updateCounter();});
 
-    toggleBinaryCounter.addEventListener("click", function(e) { e.preventDefault(); isBinary = !isBinary; updateCounter(); toggleBinaryCounter.textContent = isBinary ? "Switch to Decimal Counter" : "Switch to Binary Counter";}); 
-    updateCounter();});
+    toggleBinaryCounter.addEventListener("click", function(e) {
+        e.preventDefault(); isBinary = !isBinary; updateCounter();
+        toggleBinaryCounter.textContent = isBinary ? "Switch to Decimal Counter" : "Switch to Binary Counter";}); 
+        updateCounter();});
